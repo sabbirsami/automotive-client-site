05cc49f7e286f26e61f8a61b60cff391de6db26c
SabbirSami <smd71430@gmail.com>
2023-10-18 10:47:17 +0600

    banner section done

---
+++ src/components/home/Banner.jsx
@@ -0,0 +1,26 @@
+import bannerCar from "../../assets/hero-car.png";
+const Banner = () => {
+    return (
+        <div className=" bg-hero bg-cover bg-no-repeat">
+            <div className="container-lg bg-hero-pattern py-48">
+                <div className="grid grid-cols-5 gap-14 items-center">
+                    <div className="pb-8 col-span-2">
+                        <h1 className="text-7xl font-bold">
+                            Easy way to find the right car
+                        </h1>
+                        <p className=" py-10 text-xl opacity-70">
+                            Finder is a leading digital marketplace for the
+                            automotive industry that connects car shoppers with
+                            sellers. Also finder provides best car service ever.
+                        </p>
+                    </div>
+                    <div className="col-span-3">
+                        <img src={bannerCar} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
