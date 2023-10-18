75531bffa851459addc50851a726d19b0f9521b2
SabbirSami <smd71430@gmail.com>
2023-10-18 10:24:20 +0600

    navbar done

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/home/Navbar";
 
 const Root = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
-            hello
         </div>
     );
 };
