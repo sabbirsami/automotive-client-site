77dd08c1009489ab4d2a00c6dde805355a732cd1
SabbirSami <smd71430@gmail.com>
2023-10-19 22:53:13 +0600

    create faq section

---
+++ src/components/home/FnQ.jsx
@@ -0,0 +1,155 @@
+const FnQ = () => {
+    return (
+        <div>
+            <div className="container-lg pb-16">
+                <h1 className="text-4xl text-center pb-14">FAQs</h1>
+                <div className="grid grid-cols-2 gap-16">
+                    <div className="">
+                        <div className="join join-vertical w-full rounded-none">
+                            <div className="collapse collapse-arrow join-item rounded-none border-t border-white/40 py-4">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    What is the on-road price of the car?
+                                </div>
+                                <div className="collapse-content">
+                                    <p>
+                                        It matters simply because this is the
+                                        amount you will have to pay to buy the
+                                        car. This cost consists of the car
+                                        price, along with the price of any
+                                        accessories you have opted for,
+                                        registration, insurance, extended
+                                        warranty (if opted for) and handling
+                                        charges. It may also involve other taxes
+                                        and charges for an essential kit that
+                                        dealerships offer with every new car.
+                                        These costs, when added up, may just
+                                        push the car out of your budget. So, do
+                                        ask this question at the earliest.{" "}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-arrow join-item border-t border-white/40  py-4">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    What are the maintenance intervals and cost
+                                    per service?
+                                </div>
+                                <div className="collapse-content">
+                                    <p>
+                                        Almost every manufacturer covers the
+                                        mechanical and electrical components in
+                                        the warranty. But do check whether
+                                        accessories like stereo that are
+                                        installed by the dealer are covered
+                                        under warranty too. Also, do ask about
+                                        the intervals at which the car needs to
+                                        be serviced and for a rough estimate of
+                                        the cost of each service. This will help
+                                        you understand whether the vehicle is
+                                        maintenance-heavy or not.{" "}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-arrow join-item border-t border-white/40  py-4">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    Where is the nearest service centre?
+                                </div>
+                                <div className="collapse-content">
+                                    <p>
+                                        A lot of dealerships also double as
+                                        service centres. However, in some cases,
+                                        the dealership is separate from the
+                                        service centre. In such a case, you
+                                        should ask for the nearest service
+                                        centre. This will help you gauge how far
+                                        you need to take your car to get a
+                                        problem fixed. Also, do ask if the car
+                                        can be picked up and dropped by the
+                                        service centre guys at your doorstep.
+                                        The answer will tell you whether you
+                                        need to drop your car for service every
+                                        time or whether someone can pick it up
+                                        from your house and drop it while you
+                                        get on with your daily chores.{" "}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="">
+                        <div className="join join-vertical w-full rounded-none">
+                            <div className="collapse collapse-arrow join-item border-t border-white/40 py-4">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    Has the car been used as a demo car?
+                                </div>
+                                <div className="collapse-content">
+                                    <p>
+                                        Sometimes, dealerships have to resort to
+                                        selling demo cars due to a shortage of
+                                        supply from the manufacturer. These
+                                        cars, usually kept in mint condition,
+                                        may suffer from issues ranging from
+                                        exterior damage to mechanical issues. Do
+                                        ask this question to avoid buying a used
+                                        car for the price of a new car. As an
+                                        added precaution, you should also give
+                                        your new car a thorough look for any
+                                        issues and take it for a short spin too.{" "}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-arrow join-item border-t border-white/40 py-4">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    How soon will my car be delivered?
+                                </div>
+                                <div className="collapse-content">
+                                    <p>
+                                        Sometimes, the carmaker’s manufacturing
+                                        facility is quite close to the
+                                        dealership. In such a case, unless the
+                                        waiting period of the car&#39;s is long,
+                                        chances are that you will get your car
+                                        almost instantaneously. Sometimes, the
+                                        dealership has the same car already in
+                                        stock for you. Asking this question will
+                                        help you understand whether the dealer
+                                        has your car (in the color of your
+                                        choice) in stock or not. If you are in a
+                                        hurry to get a new car, this will help
+                                        you save time on acquiring your new car.{" "}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-arrow join-item border-t border-white/40 py-4">
+                                <input type="radio" name="my-accordion-4" />
+                                <div className="collapse-title text-xl font-medium">
+                                    How soon will my car&#39;s RC and number
+                                    plate come?
+                                </div>
+                                <div className="collapse-content">
+                                    <p>
+                                        When you buy a car, you will have to
+                                        drive it on a temporary registration
+                                        number called a Temporary Coupon or TC
+                                        for some time. The TC is valid for a
+                                        month from the date of delivery of your
+                                        vehicle. If you do not get your
+                                        car&#39;s RC within that period, it may
+                                        be impounded by the cops. So, do not
+                                        forget to ask this question.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FnQ;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import Brands from "./Brands";
 import Banner from "./Banner";
+import FnQ from "./FnQ";
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Brands />
+            <FnQ />
         </div>
     );
 };
