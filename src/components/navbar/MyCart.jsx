3e404be8ad6d2cf28afde4f7dbd5aea6b8de0429
SabbirSami <smd71430@gmail.com>
2023-10-19 20:16:06 +0600

    delete car option done

--- src/components/navbar/MyCart.jsx
+++ src/components/navbar/MyCart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../auth/AuthProvider";
+import toast from "react-hot-toast";
 
 const MyCart = () => {
     const { user } = useContext(AuthContext);
@@ -8,6 +9,48 @@ const MyCart = () => {
     const data = useLoaderData();
     const userData = data.filter((d) => d.userId === user.uid);
     console.log(userData);
+    const handleCarDelete = (id) => {
+        fetch(`http://localhost:5000/cart/${id}`, {
+            method: "DELETE",
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(result);
+                if (result.deletedCount == 1) {
+                    toast.success("Car remove successfully", {
+                        duration: 4000,
+                        position: "top-center",
+
+                        // Styling
+                        style: {
+                            marginTop: "35px",
+                        },
+
+                        // Aria
+                        ariaProps: {
+                            role: "status",
+                            "aria-live": "polite",
+                        },
+                    });
+                } else {
+                    toast.error("Remove fail", {
+                        duration: 4000,
+                        position: "top-center",
+
+                        // Styling
+                        style: {
+                            marginTop: "35px",
+                        },
+
+                        // Aria
+                        ariaProps: {
+                            role: "status",
+                            "aria-live": "polite",
+                        },
+                    });
+                }
+            });
+    };
     return (
         <div className="container-lg pb-32 pt-10">
             <h2 className="text-2xl font-semibold pb-16">My Cart</h2>
@@ -65,6 +108,16 @@ const MyCart = () => {
                                 <td>{userCart.brand}</td>
                                 <td>{userCart.type}</td>
                                 <th>${userCart?.price}</th>
+                                <th>
+                                    <button
+                                        onClick={() =>
+                                            handleCarDelete(userCart._id)
+                                        }
+                                        className="btn btn-ghost btn-xs text-[#fd5631]"
+                                    >
+                                        delete
+                                    </button>
+                                </th>
                             </tr>
                         ))}
                     </tbody>
